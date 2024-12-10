7e4a62f369b72f6a86a8d4c244673d1a5fb653e9
Musarna <131316534+Musarna@users.noreply.github.com>
2024-12-10 17:00:14 -0300

    Update views.py

--- pruebaAPI/views.py
+++ pruebaAPI/views.py
@@ -19,7 +19,7 @@ def juguete_listado(request):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.error, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(["GET","PUT","DELETE"])
 def juguete_detalle(request,pk):
